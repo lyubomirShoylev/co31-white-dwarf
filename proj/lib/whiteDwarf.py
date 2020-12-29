c36604d79d6bd6bf9b3b36d894e959c986c2dfc4
Lyubomir Shoylev <luboshoilev@gmail.com>
2020-12-29 12:10:24 +0200

    Additional documentation - class whiteDwarf.

--- proj/lib/whiteDwarf.py
+++ proj/lib/whiteDwarf.py
@@ -1,7 +1,6 @@
 """
 White dwarf class wrapper.
 """
-# TODO finish documentation
 from . import ode, derivatives
 import numpy as np
 from scipy import constants as cs       # constants for the derivative equation
@@ -21,6 +20,7 @@ class whiteDwarf(ode.ODEinit):
     span : np.ndarray
         The range of values of r to be calculated at. In normalized units to 
         cut down on computational time and improve calculation accuracy.
+        Expects an array with step size 1 between members.
     regime : {1, 2, 3}, optional
         The regime indicates the choice of equation of state -> derivative, by
         default 1 - non-relativistic.
@@ -49,8 +49,20 @@ class whiteDwarf(ode.ODEinit):
 
     Background
     ----------
-    TODO explain the normalization.
-    scaling constant in the equation, choose l^3 * 4*pi/3 = 1
+    The differential equation is a coupled system of the density `rho` and mass
+    `m` as functions of the distance from the centre `r`. These have some 
+    dimensions in powers of kg and m. We transform the variables with some
+    scaling constants so that the ODE solver works with dimensionless
+    variables. The following choice of constants is made:
+        rho = rhoC * theta,
+        m = rhoC * mu,
+        r = l * ksi,
+    where rhoC is given as input by the user and `l^3*4*pi/3 = 1`. The choice 
+    of l gives it a value on the order of 1, and we will choose it as our step
+    size in r i.e. the step size in ksi is just 1. This is a convenient choice
+    for normalization because of the initial condition on the mass:
+    `m(l) = 4/3*pi*l^3*rhoC`. In this normalization, the initial condition
+    becomes `mu(1) = 1`.
     """
 
     def __init__(self, rhoC: float, span: np.ndarray, regime: int=1) -> None:
@@ -66,7 +78,8 @@ class whiteDwarf(ode.ODEinit):
         self.const2 = (3*rhoC/16/cs.pi/cs.proton_mass *
                        (cs.h/cs.c/cs.electron_mass)**3)**(2.0/3)
 
-        # choose the equation of state and pass constants
+        # choose the equation of state and pass constants, prepares a function
+        # to be passed to the constructor of ODEinit accepting (x, y) params
         if regime == 1:
             func = lambda a, b: derivatives.nonRelativGas(a, b, self.const1)
         elif regime == 2:
@@ -78,17 +91,28 @@ class whiteDwarf(ode.ODEinit):
         super(whiteDwarf, self).__init__(init_condit, func, span)
 
     def getRadiusMass(self) -> tuple:
-        """[summary]
+        """
+        Finds the radius and mass of the white dwarf, and returns them. Takes
+        into account the normalization of the ODE. 
+
+        The function scans `yOut[:,0]` column containing `rho(r)` values. We
+        know that this is a decreasing function; the first instance where it 
+        has a value <= 0 it where the star "ends".
 
         Returns
         -------
         tuple
-            [description]
+            Returns a tuple `(Radius, Mass)`. Radius and Mass are calculated
+            from `yOut` with the appropriate normalization constants. For
+            Radius it is `l = (3/(4*pi))^(1/3)`, result is in m. For Mass it is
+            `rhoC`, result is in kg.
         """
 
         # find index of rho = 0 (changing signs)
         if self.flagIntegr:
             index = np.searchsorted(-self.yOut[:, 0], 0)
+            # index is the position where we have yOut[index,:] ~ [0,0]
+
             l = (3/(4*np.pi))**(1.0/3.0)                # normalization const
             self.Radius = (index-1)*l                   # in m
             self.Mass = self.yOut[index-1, 1]*self.rhoC # in kg*m^(-3)
